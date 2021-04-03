1e244953a26c701e2eb3097a420e7045d6d5bf0f
MinoSilvia <silviaisabel.mino@gmail.com>
2021-04-03 19:27:47 -0300

    implementacion de los metodos del service y el controller.

---
+++ rrhh/src/main/java/ar/com/ada/api/rrhh/entities/Categoria.java
@@ -0,0 +1,62 @@
+package ar.com.ada.api.rrhh.entities;
+
+import java.math.BigDecimal;
+import java.util.*;
+
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+
+    @Id
+    @Column(name = "categoria_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int CategoriaId;
+    @Column(name = "nombre")
+    private String nombre;
+    @Column(name = "sueldo_base")
+    private BigDecimal sueldoBase;
+    @OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL) // ,fetch = FetchType.EAGER)
+    @LazyCollection(LazyCollectionOption.FALSE)
+    @JsonIgnore
+    private List<Empleado> empleados;
+
+    public int getCategoriaId() {
+        return CategoriaId;
+    }
+
+    public void setCategoriaId(int categoriaId) {
+        CategoriaId = categoriaId;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public BigDecimal getSueldoBase() {
+        return sueldoBase;
+    }
+
+    public void setSueldoBase(BigDecimal sueldoBase) {
+        this.sueldoBase = sueldoBase;
+    }
+
+    public List<Empleado> getEmpleados() {
+        return empleados;
+    }
+
+    public void setEmpleados(List<Empleado> empleados) {
+        this.empleados = empleados;
+    }
+
+}
